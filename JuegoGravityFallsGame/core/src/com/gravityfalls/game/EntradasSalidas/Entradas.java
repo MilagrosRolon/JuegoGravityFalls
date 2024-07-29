1e50a0209a33d2d9346a61b212a0ed97feaeef2e
MilagrosRolon <milagrosrolon959@gmail.com>
2024-07-28 23:52:41 -0300

    Update Entradas.java

--- JuegoGravityFallsGame/core/src/com/gravityfalls/game/EntradasSalidas/Entradas.java
+++ JuegoGravityFallsGame/core/src/com/gravityfalls/game/EntradasSalidas/Entradas.java
@@ -1,20 +1,22 @@
 package com.gravityfalls.game.EntradasSalidas;
 
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.gravityfalls.game.pantallas.PantallaLogo;
 import com.gravityfalls.game.pantallas.PantallaMenu;
+import com.gravityfalls.game.utiles.Config;
 
 public class Entradas implements InputProcessor {
 
     private PantallaLogo pantallaLogo;
-    private PantallaMenu app;
-    private boolean abajo = false, arriba = false;
-    private boolean enter = false;
+    private PantallaMenu pantallaMenu;
+    private boolean abajo = false, arriba = false, enter =false,click= false;
+    private int mouseX = 0, mouseY = 0;
+
     // Constructor para PantallaMenu
-    public Entradas(PantallaMenu app) {
-        this.app = app;
+    public Entradas(PantallaMenu pantallaMenu) {
+        this.pantallaMenu = pantallaMenu;
     }
 
     // Constructor para PantallaLogo
@@ -24,33 +26,36 @@ public class Entradas implements InputProcessor {
 
     @Override
     public boolean keyDown(int keycode) {
-        app.tiempo = 0.08f;
-       
+        if (pantallaMenu != null) {
+            pantallaMenu.tiempo = 0.08f;
+        }
+
         if (keycode == Keys.DOWN) {
             abajo = true;
         } else if (keycode == Keys.UP) {
             arriba = true;
         }
         if (keycode == Keys.ENTER) {
-           enter = true;
+            enter = true;
+            if (pantallaLogo != null) {
+                pantallaLogo.terminarPantalla();
+            }
         }
-        
+
         return false;
     }
 
     @Override
     public boolean keyUp(int keycode) {
-    	
-
         if (keycode == Keys.DOWN) {
             abajo = false;
         } else if (keycode == Keys.UP) {
             arriba = false;
         }
         if (keycode == Keys.ENTER) {
-           enter = false;
+            enter = false;
         }
-     
+
         return false;
     }
 
@@ -61,12 +66,15 @@ public class Entradas implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
+        click= true;
+    	return false;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
+       click =false;
+    	return false;
+        
     }
 
     @Override
@@ -81,6 +89,9 @@ public class Entradas implements InputProcessor {
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
+        // Ajustar la coordenada Y si es necesario
+        mouseX = screenX;
+        mouseY = Config.ALTO - screenY; // Gdx.graphics.getHeight() en lugar de Config.ALTO
         return false;
     }
 
@@ -88,7 +99,7 @@ public class Entradas implements InputProcessor {
     public boolean scrolled(float amountX, float amountY) {
         return false;
     }
-    
+
     public boolean isAbajo() {
         return abajo;
     }
@@ -97,8 +108,20 @@ public class Entradas implements InputProcessor {
         return arriba;
     }
 
-	public boolean isEnter() {
-		
-		return enter;
+    public boolean isEnter() {
+        return enter;
+    }
+
+    public int getMouseX() {
+        return mouseX;
+    }
+
+    public int getMouseY() {
+        return mouseY;
+    }
+
+	public boolean isClick() {
+		return click;
 	}
+
 }
