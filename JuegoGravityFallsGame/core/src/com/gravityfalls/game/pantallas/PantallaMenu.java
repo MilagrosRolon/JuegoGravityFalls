0a119abe6a5cc1f02b4f264a2f6cf21a226718f4
MilagrosRolon <milagrosrolon959@gmail.com>
2024-07-28 23:53:15 -0300

    Update PantallaMenu.java

--- JuegoGravityFallsGame/core/src/com/gravityfalls/game/pantallas/PantallaMenu.java
+++ JuegoGravityFallsGame/core/src/com/gravityfalls/game/pantallas/PantallaMenu.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.gravityfalls.game.EntradasSalidas.Entradas;
 import com.gravityfalls.game.elementos.Imagen;
 import com.gravityfalls.game.elementos.Texto;
@@ -13,97 +15,112 @@ import com.gravityfalls.game.utiles.Render;
 
 public class PantallaMenu implements Screen {
 
-	Imagen fondo;
-	SpriteBatch b;
-	
-	Texto opciones[] = new Texto[3];
-	String  textos[] = {"EMPEZAR BUSQUEDA","COMO JUGAR","CERRAR"};
-	
-	Entradas entradas = new Entradas(this);
-	
-	int opcion = 1;
-	public float tiempo = 0;
+    Imagen fondo;
+    SpriteBatch b;
+    Texto opciones[] = new Texto[3];
+    String textos[] = {"EMPEZAR BUSQUEDA", "COMO JUGAR", "CERRAR"};
 
-	@Override
-	public void show() {
-	    fondo = new Imagen(Recursos.MENU);
-	    b = Render.batch;
-	    
-	    Gdx.input.setInputProcessor(entradas);
-	    
-	    int avance = 45;
-	    
-	    for (int i = 0; i < opciones.length; i++) {
-	        opciones[i] = new Texto(Recursos.FUENTE, 35, Color.WHITE, true);
-	        opciones[i].setTexto(textos[i]);
-	        opciones[i].setPosition(
-	            (Config.ANCHO / 2) - (opciones[i].getAncho() / 2),
-	            ((Config.ALTO / 2) + (opciones[0].getAlto()/2))-((opciones[i].getAlto()*i)+(i * avance))
-	        );
-	    }
-	}
+    int opcion = 1;
+    public float tiempo = 0;
+    ShapeRenderer sr;
+    Entradas entradas;
 
-	@Override
-	public void render(float delta) {
-	    b.begin();
-	    fondo.dibujar();
-	    for (int i = 0; i < opciones.length; i++) {
-	        opciones[i].dibujar();
-	    }
-	    b.end();
-	    
-	    tiempo += delta;
+    @Override
+    public void show() {
+        fondo = new Imagen(Recursos.MENU);
+        b = Render.batch;
 
-	    if (entradas.isAbajo() && tiempo > 0.09f) {
-	        opcion++;
-	        if (opcion > 3) {
-	            opcion = 1;
-	        }
-	        tiempo = 0;
-	    } else if (entradas.isArriba() && tiempo > 0.09f) {
-	        opcion--;
-	        if (opcion < 1) {
-	            opcion = 3;
-	        }
-	        tiempo = 0;
-	    }
-	
-	 for (int i = 0; i < opciones.length; i++) {
-	       if(i == opcion-1) {
-	    	   opciones[i].setColor(Color.VIOLET);
-	       } else {
-	    	   opciones[i].setColor(Color.WHITE);
-	       }
-	    }
-	 
-		if(entradas.isEnter()) {
-			if(opcion == 1) {
-				Render.app.setScreen(new PantallaEmpezarJuego());
-			}
-		}
+        entradas = new Entradas(this);
+        Gdx.input.setInputProcessor(entradas);
 
-			
-	}
-	
-	
-	@Override
-	public void resize(int width, int height) {
-	}
+        int avance = 35;
 
-	@Override
-	public void pause() {
-	}
+     
 
-	@Override
-	public void resume() {
-	}
+        for (int i = 0; i < opciones.length; i++) {
+            opciones[i] = new Texto(Recursos.FUENTE, 35, Color.WHITE, true);
+            opciones[i].setTexto(textos[i]);
+            opciones[i].setPosition(
+                (Config.ANCHO / 2) - (opciones[i].getAncho() / 2),
+                ((Config.ALTO / 2) + (opciones[0].getAlto() / 2)) - ((opciones[i].getAlto() * i) + (i * avance))
+            );
+        }
+    }
 
-	@Override
-	public void hide() {
-	}
+    @Override
+    public void render(float delta) {
+        tiempo += delta;
 
-	@Override
-	public void dispose() {
-	}
+        // Actualiza la opción seleccionada en función de la posición del ratón
+        for (int i = 0; i < opciones.length; i++) {
+            if (entradas.getMouseX() >= opciones[i].getX() &&
+                entradas.getMouseX() <= opciones[i].getX() + opciones[i].getAncho() &&
+                entradas.getMouseY() >= opciones[i].getY() - opciones[i].getAlto() &&
+                entradas.getMouseY() <= opciones[i].getY()) {
+                
+                opcion = i + 1; // Actualiza la opción seleccionada
+            }
+        }
 
+        // Actualiza el color de las opciones basado en la opción seleccionada
+        for (int i = 0; i < opciones.length; i++) {
+            if (i == opcion - 1) {
+                opciones[i].setColor(Color.VIOLET);
+            } else {
+                opciones[i].setColor(Color.WHITE);
+            }
+        }
+
+        // Dibuja todo
+        b.begin();
+        fondo.dibujar();
+        for (int i = 0; i < opciones.length; i++) {
+            opciones[i].dibujar();
+        }
+
+        b.end();
+
+
+        // Manejo de la entrada del teclado
+        if (entradas.isAbajo() && tiempo > 0.09f) {
+            opcion++;
+            if (opcion > 3) {
+                opcion = 1;
+            }
+            tiempo = 0;
+        } else if (entradas.isArriba() && tiempo > 0.09f) {
+            opcion--;
+            if (opcion < 1) {
+                opcion = 3;
+            }
+            tiempo = 0;
+        }
+
+        // Manejo del ENTER
+        if (entradas.isEnter()||entradas.isClick()) {
+            if (opcion == 1) {
+                Render.app.setScreen(new PantallaEmpezarJuego());
+            }
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
