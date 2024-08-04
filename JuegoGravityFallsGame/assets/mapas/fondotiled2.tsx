b70a44d2236ccbba0e9339b8db9e934c1a57176b
MilagrosRolon <milagrosrolon959@gmail.com>
2024-08-04 01:35:08 -0300

    Delete JuegoGravityFallsGame directory

--- JuegoGravityFallsGame/assets/mapas/fondotiled2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="fondotiled2" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="2769" columns="71">
- <image source="fondotiled2.jpeg" width="1280" height="718"/>
-</tileset>
--- JuegoGravityFallsGame/core/src/accesoriosMapa/Suelo.java
+++
@@ -1,5 +0,0 @@
-package accesoriosMapa;
-
-public class Suelo {
-
-}
